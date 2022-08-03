3dbe55e59dff4bb0ce3bc44b7eb404dfad6d0868
krzysztof.matyjaszczyk <krzysiek.matyjaszczyk@gmail.com>
2022-08-03 20:37:04 +0200

    Initial commit

---
+++ src/test/java/pl/itkurnik/skirental/SkiRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.itkurnik.skirental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkiRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
