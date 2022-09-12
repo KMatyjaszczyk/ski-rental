9a3c8243ec0538d55132994ce7b827f92f4e649e
krzysztof.matyjaszczyk <krzysiek.matyjaszczyk@gmail.com>
2022-09-12 19:48:25 +0200

    Move auth endpoints to another Controller

---
+++ src/main/java/pl/itkurnik/skirental/domain/auth/AuthController.java
@@ -0,0 +1,57 @@
+package pl.itkurnik.skirental.domain.auth;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.itkurnik.skirental.domain.user.UserLoginService;
+import pl.itkurnik.skirental.domain.user.UserService;
+import pl.itkurnik.skirental.domain.user.dto.CreateRegisteredUserRequest;
+import pl.itkurnik.skirental.domain.user.dto.TokenValidationResponse;
+import pl.itkurnik.skirental.domain.user.exception.IncorrectUserNameOrPasswordException;
+import pl.itkurnik.skirental.security.model.AuthenticationRequest;
+import pl.itkurnik.skirental.security.model.AuthenticationResponse;
+import pl.itkurnik.skirental.security.util.MyUserDetailsService;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+@Slf4j
+public class AuthController {
+    private final UserService userService;
+    private final UserLoginService userLoginService;
+    private final MyUserDetailsService userDetailsService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
+        log.info("Logging in with username {}", request.getUsername());
+
+        try {
+            String token = userLoginService.loginAndGetJwt(request);
+            return ResponseEntity.ok(new AuthenticationResponse(token));
+        } catch (IncorrectUserNameOrPasswordException e) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); // TODO KM API Exceptions handling
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody CreateRegisteredUserRequest request) {
+        log.info("Registering user {} {}", request.getName(), request.getSurname());
+
+        userService.registerUser(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/validate")
+    public ResponseEntity<TokenValidationResponse> validateToken(@RequestBody String token) {
+        log.info("Validating token {}", token);
+        return ResponseEntity.ok(new TokenValidationResponse(userDetailsService.validateToken(token)));
+    }
+}
--- src/main/java/pl/itkurnik/skirental/domain/user/UserController.java
+++ src/main/java/pl/itkurnik/skirental/domain/user/UserController.java
@@ -2,46 +2,16 @@ package pl.itkurnik.skirental.domain.user;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.itkurnik.skirental.domain.user.dto.CreateRegisteredUserRequest;
-import pl.itkurnik.skirental.domain.user.exception.IncorrectUserNameOrPasswordException;
-import pl.itkurnik.skirental.security.model.AuthenticationRequest;
-import pl.itkurnik.skirental.security.model.AuthenticationResponse;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/user")
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
     private final UserService userService;
-    private final UserLoginService userLoginService;
 
-    @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
-        log.info("Logging in with username {}", request.getUsername());
+    // TODO KM create unregistered user
 
-        try {
-            String token = userLoginService.loginAndGetJwt(request);
-            return ResponseEntity.ok(new AuthenticationResponse(token));
-        } catch (IncorrectUserNameOrPasswordException e) {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); // TODO KM API Exceptions handling
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity<Void> register(@RequestBody CreateRegisteredUserRequest request) {
-        log.info("Registering user {} {}", request.getName(), request.getSurname());
-
-        userService.registerUser(request);
-        return ResponseEntity.ok().build();
-    }
-
-    // TODO do token validation
 }
