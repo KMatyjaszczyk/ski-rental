7ddb8bff01aba484e5baa5d0026c7eba4d14af1d
krzysztof.matyjaszczyk <krzysiek.matyjaszczyk@gmail.com>
2022-10-17 23:37:19 +0200

    [EQUIPMENT] Add repository package

--- src/main/java/pl/itkurnik/skirental/domain/equipment/repository/EquipmentCategoryRepository.java
+++ src/main/java/pl/itkurnik/skirental/domain/equipment/repository/EquipmentCategoryRepository.java
@@ -1,6 +1,7 @@
-package pl.itkurnik.skirental.domain.equipment;
+package pl.itkurnik.skirental.domain.equipment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.itkurnik.skirental.domain.equipment.EquipmentCategory;
 
 public interface EquipmentCategoryRepository extends JpaRepository<EquipmentCategory, Integer> {
 }
--- src/main/java/pl/itkurnik/skirental/domain/equipment/repository/ManufacturerRepository.java
+++ src/main/java/pl/itkurnik/skirental/domain/equipment/repository/ManufacturerRepository.java
@@ -1,6 +1,7 @@
-package pl.itkurnik.skirental.domain.equipment;
+package pl.itkurnik.skirental.domain.equipment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.itkurnik.skirental.domain.equipment.Manufacturer;
 
 public interface ManufacturerRepository extends JpaRepository<Manufacturer, Integer> {
 }
