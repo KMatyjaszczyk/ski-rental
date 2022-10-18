541e826fcce4aea6c1b9ce414bd025587474bea6
krzysztof.matyjaszczyk <krzysiek.matyjaszczyk@gmail.com>
2022-10-18 20:42:53 +0200

    [CORE] Add CORS constants, make constants final

--- src/main/java/pl/itkurnik/skirental/api/Constants.java
+++ src/main/java/pl/itkurnik/skirental/api/Constants.java
@@ -1,11 +1,13 @@
 package pl.itkurnik.skirental.api;
 
 public final class Constants {
-    public static String EMPLOYEE_AUTH_NAME = "EMPLOYEE";
-    public static String AUTHORIZATION_HEADER = "Authorization";
-    public static String BEARER_TOKEN_BEGIN = "Bearer ";
-    public static int MINIMAL_PASSWORD_LENGTH = 7;
-    public static int PHONE_NUMBER_LENGTH = 9;
-    public static int PHONE_NUMBER_WITH_PREFIX_LENGTH = 13;
-    public static String UNEXPECTED_ERROR_MESSAGE = "Unexpected error occurred";
+    public static final String EMPLOYEE_AUTH_NAME = "EMPLOYEE";
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_TOKEN_BEGIN = "Bearer ";
+    public static final int MINIMAL_PASSWORD_LENGTH = 7;
+    public static final int PHONE_NUMBER_LENGTH = 9;
+    public static final int PHONE_NUMBER_WITH_PREFIX_LENGTH = 13;
+    public static final String UNEXPECTED_ERROR_MESSAGE = "Unexpected error occurred";
+    public static final String LOCALHOST_FRONTEND_APP_URL = "http://localhost:3000";
+    public static final int CROSS_ORIGIN_MAX_AGE = 3600;
 }
