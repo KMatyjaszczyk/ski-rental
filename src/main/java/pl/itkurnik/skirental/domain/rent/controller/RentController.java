ab51d703d41181787a3df25080036a924ada5d77
krzysztof.matyjaszczyk <krzysiek.matyjaszczyk@gmail.com>
2022-11-23 18:36:11 +0100

    [RENT] Add error handling while creating rent

--- src/main/java/pl/itkurnik/skirental/domain/rent/controller/RentController.java
+++ src/main/java/pl/itkurnik/skirental/domain/rent/controller/RentController.java
@@ -6,8 +6,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import pl.itkurnik.skirental.api.Constants;
 import pl.itkurnik.skirental.domain.rent.dto.CreateRentRequest;
+import pl.itkurnik.skirental.domain.rent.exception.CreateRentValidationException;
 import pl.itkurnik.skirental.domain.rent.service.RentService;
+import pl.itkurnik.skirental.util.error.ObjectNotFoundException;
 
 import static pl.itkurnik.skirental.api.Constants.CROSS_ORIGIN_MAX_AGE;
 import static pl.itkurnik.skirental.api.Constants.LOCALHOST_FRONTEND_APP_URL;
@@ -27,9 +30,15 @@ public class RentController {
             rentService.create(request);
             log.info("Rent for client with document {} created successfully", request.getClientDocumentNumber());
             return ResponseEntity.ok().build();
+        } catch (CreateRentValidationException | ObjectNotFoundException e) {
+            log.info(e.getMessage(), e);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        } catch (IllegalStateException e) {
+            log.info(e.getMessage(), e);
+            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage(), e);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e); // TODO KM insert default message here
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, Constants.UNEXPECTED_ERROR_MESSAGE, e);
         }
     }
 }
